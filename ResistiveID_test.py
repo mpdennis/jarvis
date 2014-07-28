3282b8891c90deac4684a3b73a77591c40be51b0
Michael <mpd9394@yahoo.com>
2014-07-28 09:44:00 -0600

    ResistiveID

--- ResistiveID_test.py
+++ ResistiveID_test.py
@@ -19,9 +19,9 @@ while True:
 		print 2
 	elif(0.2535 < volts <= 0.455):
 		print 3
-	elif(0.455 < volts <= .592):
+	elif(0.455 < volts <= .582):
 		print 4
-	elif(0.592 < volts <= 0.737):
+	elif(0.582 < volts <= 0.737):
 		print 5
         elif(0.737 < volts <= 0.95):
 		print 6
@@ -29,23 +29,23 @@ while True:
 		print 7
 	elif(1.14 < volts <= 1.29):
 		print 8
-        elif(1.29 < volts <= 1.43):
+        elif(1.29 < volts <= 1.40):
 		print 9
-        elif(1.43 < volts <= 1.625):
+        elif(1.40 < volts <= 1.625):
 		print 10
-	elif(1.625 < volts <= 1.835):
+	elif(1.625 < volts <= 1.795):
 		print 11
-        elif(1.835 < volts <= 2.01):
+        elif(1.795 < volts <= 1.96):
 		print 12
-        elif(2.01 < volts <= 2.19):
+        elif(1.96 < volts <= 2.15):
 		print 13
-	elif(2.19 < volts <= 2.375):
+	elif(2.15 < volts <= 2.325):
 		print 14
-        elif(2.375 < volts <= 2.555):
+        elif(2.325 < volts <= 2.505):
 		print 15
-        elif(2.555 < volts <= 2.704):
+        elif(2.505 < volts <= 2.654):
 		print 16
-	elif(2.704 < volts <= 2.8725):
+	elif(2.654 < volts <= 2.8725):
 		print 17
         elif(2.8745 < volts <= 3.0635):
 		print 18
